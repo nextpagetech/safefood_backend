ee488d0366e02f6de6e60c9eff12a9ad6f8f5ce8
nadikatlanikhila <nadikatlanikhila@gmail.com>
2024-07-01 19:09:45 +0530

    vendor

--- routes/vendor_products.js
+++ routes/vendor_products.js
@@ -5,13 +5,14 @@ const {
     getAllvendor_product,
     getvendor_productId,
     updatevendor_product,
+    deletevendor_product,
 } = require("../controller/vendor_products");
 
 //add a vendor
 router.post("/vendor_productadd", vendor_productadd);
 
 //get all vendor
-router.get("/getvendor_product", getAllvendor_product);
+router.get("/getAllvendor_product", getAllvendor_product);
 
 //get a vendor
 router.get("/:id", getvendor_productId);
@@ -19,6 +20,6 @@ router.get("/:id", getvendor_productId);
 //update a vendor
 router.put("/:id", updatevendor_product);
 
-
+router.delete("/:id", deletevendor_product);
 
 module.exports = router;
